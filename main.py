da21cadd230cc42dd9d9d204585e69454a6be68c
Bharghavi <bhagisr313@gmail.com>
2026-06-09 19:07:36 -0400

    merge two sortted lists

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from leetcode_easy_problems.p0136_single_number import Solution
+from l3_linked_list.p0026_merge_two_sorted_lists import Solution,ListNode
 
 def main():
-    nums = [2,2,1]
-    result = Solution().singleNumber(nums)
+    list1 = ListNode(1,ListNode(2,ListNode(4,None)))
+    list2 = ListNode(1,ListNode(3,ListNode(4,None)))
+    result = Solution().mergeTwoLists(list1,list2)
     print(result)
 
 if __name__ == "__main__":
